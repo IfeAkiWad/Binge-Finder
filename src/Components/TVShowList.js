0e7d0144b71f9cd4bb93a15870c3bde3270c0a0a
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-08-10 23:16:10 -0500

    add props

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -4,11 +4,13 @@ import TVShow from './TVShow'
 
 class TVShowList extends Component {
 
+  // eslint-disable-next-line
   mapAllShows = (props) => {
     if (!!props.searchTerm){
+      // eslint-disable-next-line
       return props.shows.map((s) => {
         if (s.name.toLowerCase().includes(props.searchTerm)){
-         return (<TVShow show={s} key={s.id} /*selectShow={selectShow}*//> ) //where is selectShow coming from?
+         return (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> ) 
         }
       })
     }
